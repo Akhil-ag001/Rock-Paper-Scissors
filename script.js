06b8ecc62dc7f453592931051ff02f4a529fea0e
Akhil <akhilagarwal1205@gmail.com>
2023-03-24 22:04:01 +0530

    added player choice func.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let playerscore = 0;
 let computerscore = 0;
 let computerChoice = getComputerChoice();
 console.log(computerChoice);
+let playerChoice = getPlayerChoice();
+console.log(playerChoice);
 
 
 function getComputerChoice(){
@@ -13,4 +15,11 @@ function getComputerChoice(){
         return "Paper";
     else
         return "Scissors";
+}
+
+function getPlayerChoice(){
+    let choice = prompt("Enter your choice- ");
+    choice = choice.toLowerCase();
+    choice = choice.charAt(0).toUpperCase() + choice.slice(1);
+    return choice;
 }
